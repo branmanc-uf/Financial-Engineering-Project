4bbb63573fe6d38ed6c41b4d5e3b577958738c13
Antonio <antoniodeguzman2004@gmail.com>
2025-03-09 04:49:55 -0400

    made candlestick functionality work on jupyter

--- generate_candlestick_df.py
+++ generate_candlestick_df.py
@@ -5,9 +5,6 @@ def generate_candlestick(simulated_data):
     # Simulate stock data
     df = simulated_data
 
-
-    print(df)
-
     # Initialize lists to store the aggregated data
     open_list = []
     close_list = []
@@ -15,10 +12,11 @@ def generate_candlestick(simulated_data):
     low_list = []
     volume_list = []
     day_list = []
+    timestamp_list = []
 
     # Iterate through the dataframe in chunks of 5
-    for i in range(0, len(df), 5):
-        chunk = df.iloc[i:i+5]
+    for i in range(0, len(df), 30):
+        chunk = df.iloc[i:i+30]
         if not chunk.empty:
             open_list.append(chunk['Close'].iloc[0])
             close_list.append(chunk['Close'].iloc[-1])
@@ -26,9 +24,11 @@ def generate_candlestick(simulated_data):
             low_list.append(chunk['Close'].min())
             volume_list.append(chunk['Volume'].sum())
             day_list.append(chunk['Day'].sum() // 5)  # Extract the day from the index
+            timestamp_list.append(chunk['Timestamp'].iloc[0])
 
     # Create a new dataframe with the aggregated data
     df_resampled = pd.DataFrame({
+        'Timestamp': timestamp_list,
         'Open': open_list,
         'Close': close_list,
         'High': high_list,
@@ -42,6 +42,7 @@ def generate_candlestick(simulated_data):
 # Example usage
 if __name__ == "__main__":
     days = 2  # Specify the number of days for simulation
-    candlestick_df = generate_candlestick(simulate_stock(days))
+    fake_stock, nothingOne, nothingTwo = simulate_stock(days)
+    candlestick_df = generate_candlestick(fake_stock)
     print("this is the candlestick df")
     print(candlestick_df)
